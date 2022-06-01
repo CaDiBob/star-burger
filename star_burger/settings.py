925f5e87227d47b76829e91dbc17f1512060cc2f
CaDiBob <nikolay.v.byankin@gmail.com>
2022-06-01 19:08:20 +0900

    Добавил растояния от ресторана до объекта

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -8,6 +8,8 @@ from environs import Env
 env = Env()
 env.read_env()
 
+YANDEX_GEOCODER_API = env('YANDEX_GEOCODER_API')
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
