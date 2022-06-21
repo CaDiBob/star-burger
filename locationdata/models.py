d4c5fe5aae96b0e4353c2ff010b23f3fdfe3609e
CaDiBob <nikolay.v.byankin@gmail.com>
2022-06-22 00:25:06 +0900

    Разрешил полям остоваться пустыми

--- locationdata/models.py
+++ locationdata/models.py
@@ -3,9 +3,14 @@ from django.utils import timezone
 
 
 class Location(models.Model):
-    address = models.CharField('Адрес', max_length=100, db_index=True, unique=True)
-    lat = models.FloatField('Широта', db_index=True)
-    lon = models.FloatField('Долгота', db_index=True)
+    address = models.CharField(
+        'Адрес',
+        max_length=100,
+        db_index=True,
+        unique=True
+    )
+    lat = models.FloatField('Широта', db_index=True, blank=True)
+    lon = models.FloatField('Долгота', db_index=True, blank=True)
     created_at = models.DateField('Дата', default=timezone.now, db_index=True)
 
     class Meta:
