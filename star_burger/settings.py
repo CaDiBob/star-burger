c98a6b2c102a4e4c160098db63961a4edbb1ba9a
Admin <nikolay.v.byankin@gmail.com>
2022-06-27 12:27:50 +0000

    install gunicorn instal nginx

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -15,9 +15,9 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 SECRET_KEY = env('SECRET_KEY')
-DEBUG = env.bool('DEBUG', True)
+DEBUG = env.bool('DEBUG')
 
-ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
+ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost',  '77.223.97.56'])
 
 INSTALLED_APPS = [
     'foodcartapp.apps.FoodcartappConfig',
