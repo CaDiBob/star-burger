1a246205e6ccdfe9b561dbc9e44c5d51e60933ec
CaDiBob <nikolay.v.byankin@gmail.com>
2022-10-13 20:00:49 +0900

    Изменил структуру папок

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG')
 
 ALLOWED_HOSTS = env.list(
-    'ALLOWED_HOSTS', ['127.0.0.1', 'localhost', '77.223.97.56', 'cadburger.tk',]
+    'ALLOWED_HOSTS', ['127.0.0.1', 'localhost']
 )
 
 INSTALLED_APPS = [
@@ -136,7 +136,7 @@ INTERNAL_IPS = [
 ]
 
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "assets"),
-    os.path.join(BASE_DIR, "bundles"),
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "assets"),
+#     os.path.join(BASE_DIR, "bundles"),
+# ]
--- star_burger/urls.py
+++ star_burger/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('api/', include('foodcartapp.urls')),
     path('manager/', include('restaurateur.urls')),
     path('api-auth/', include('rest_framework.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
 
 if settings.DEBUG:
     import debug_toolbar
