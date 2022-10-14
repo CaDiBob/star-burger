7b01b17a23bdb6dd700b31141c7bb309c774c97d
CaDiBob <nikolay.v.byankin@gmail.com>
2022-10-14 20:47:02 +0900

    Добавил файл для сборки фронта

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -19,7 +19,7 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG')
 
 ALLOWED_HOSTS = env.list(
-    'ALLOWED_HOSTS', ['127.0.0.1', 'localhost']
+    'ALLOWED_HOSTS', ['127.0.0.1', 'localhost', '81.163.26.170']
 )
 
 INSTALLED_APPS = [
