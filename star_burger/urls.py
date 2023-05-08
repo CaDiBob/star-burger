8fa6e82be52fe6b26958931cd22e4632188c8e59
CaDiBob <nikolay.v.byankin@gmail.com>
2023-05-08 20:39:41 +0900

    corrected dev-file docker compose

--- star_burger/urls.py
+++ star_burger/urls.py
@@ -18,3 +18,5 @@ if settings.DEBUG:
     urlpatterns = [
         path(r'__debug__/', include(debug_toolbar.urls)),
     ] + urlpatterns
+
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
